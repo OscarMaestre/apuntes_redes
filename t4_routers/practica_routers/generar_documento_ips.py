48a00c7a9a1c298292433f3926e1caafa89bad12
Oscar GG <oscar@nomail.com>
2022-04-01 22:38:11 +0200

    Imagenes listas

--- t4_routers/practica_routers/generar_documento_ips.py
+++ t4_routers/practica_routers/generar_documento_ips.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from os import mkdir
+from os.path import join
+from utilidades.imagenes.CreadorImagenes import CreadorImagenes
 
 TARJETA_1="enp0s3"
 TARJETA_2="enp0s8"
@@ -16,8 +19,8 @@ network:
 class Router(object):
     def __init__(self):
         self.tarjetas=dict()
-    def asignar_ip_tarjeta(self, ip, tarjeta):
-        self.tarjetas[ip]=tarjeta
+    def asignar_ip_tarjeta(self, tarjeta, ip):
+        self.tarjetas[tarjeta]=ip
     def get_ip_tarjeta(self, tarjeta):
         nombre_tarjeta="    {0}:\n".format(tarjeta)
         ip            ="      addresses: [{0}]\n".format(self.tarjetas[tarjeta])
@@ -28,6 +31,12 @@ class Router(object):
         for clave in self.tarjetas:
             ips.append(self.get_ip_tarjeta(clave))
         return "".join(ips)
+
+    def get_lista_ips(self):
+        ips=[self.tarjetas[TARJETA_1], self.tarjetas[TARJETA_2], self.tarjetas[TARJETA_3]]
+        
+        return ips
+
     def get_fichero_netplan(self):
         ips=self.get_todas_tarjetas()
         return FICHERO_NETPLAN.format(ips)
@@ -38,9 +47,9 @@ class Router(object):
         return ",".join(ips)
 
 def generar_lista_routers(num_equipo):
-    T3="enp0s3"
-    T8="enp0s8"
-    T9="enp0s9"
+    T3=TARJETA_1
+    T8=TARJETA_2
+    T9=TARJETA_3
     red_base=100+num_equipo
     cad_red_base=str(red_base)
     r1=Router()
@@ -52,7 +61,8 @@ def generar_lista_routers(num_equipo):
     r1.asignar_ip_tarjeta(T9, "3."+cad_red_base+".0.1/16")
 
 
-    r2.asignar_ip_tarjeta(T3, "4."+cad_red_base+".0.1/16")
+    #r2.asignar_ip_tarjeta(T3, "4."+cad_red_base+".0.1/16")
+    r2.asignar_ip_tarjeta(T3, "Reservada")
     r2.asignar_ip_tarjeta(T8, "2."+cad_red_base+".0.2/16")
     r2.asignar_ip_tarjeta(T9, "5."+cad_red_base+".0.1/16")
 
@@ -67,11 +77,59 @@ def generar_lista_routers(num_equipo):
     
     return (r1,r2,r3)
 
+
+def get_nombre_subdirectorio(num_equipo):
+    nombre_subdirectorio="PC-"+str(num_equipo).zfill(2)
+    directorio=join("Documentos", nombre_subdirectorio)
+    return directorio
+
+def get_lista_ips(datos_ips):
+    ips=[]
+    (ip1, tupla_routers,ip2)=datos_ips
+    (r1, r2, r3)=tupla_routers
+    ips.append(ip1)
+    lista2=r1.get_lista_ips()+r2.get_lista_ips()+r3.get_lista_ips()
+    ips=ips+lista2
+    ips.append(ip2)
+
+    return ips
+
+def generar_imagen(num_equipo, datos_ips):
+    IMG_ORIGINAL="Router3.png"
+    SUBDIRECTORIO=get_nombre_subdirectorio(num_equipo)
+    IMG_FINAL=join(SUBDIRECTORIO,"Red.png")
+    ips=get_lista_ips(datos_ips)
+
+    coordenadas=[
+        #IP1
+        (15,166),
+        #R1 enp0s3    enp0s8     enp0s9
+        (89,262), (161,134), (155,291),
+        #R2 enp0s3    enp0s8     enp0s9
+        (536,42), (305,37), (482, 107),
+        #R3 enp0s3    enp0s8     enp0s9
+        (396,370), (298,338),(475,230),
+        #IP2
+        (553,315)
+    ]
+    creador=CreadorImagenes(IMG_ORIGINAL, IMG_FINAL, tamano_fuente=14)
+    creador.poner_textos(ips, coordenadas)
+    creador.guardar()
+
 def generar_para_equipo(num_equipo):
     routers=generar_lista_routers(num_equipo)
     ip1="1."+str(num_equipo)+".101.10"
     ip2="6."+str(num_equipo)+".101.10"
-    return (ip1, routers, ip2)
+    
+    #Fabricamos el subdirectorio
+    try:
+        mkdir(get_nombre_subdirectorio(num_equipo))
+    except FileExistsError:
+        pass
+    datos_ips=(ip1, routers, ip2)
+    generar_imagen(num_equipo, datos_ips)
+
+    return datos_ips
 
 def generar_documento():
     texto=""
